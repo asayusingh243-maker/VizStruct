f1dab1e8c7c1c019817768fa194b3bea47db9d8e
harshita-yadav8 <harshi.yadav4992@gmail.com>
2026-09-18 13:20:25 +0530

    Delete roadmapRoutes.js

--- roadmapRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getRoadmap } = require("../controllers/roadmapController");
-const protect = require("../middleware/authMiddleware");
-
-router.get("/", protect, getRoadmap);
-
-module.exports = router;
