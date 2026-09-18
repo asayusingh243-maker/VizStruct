55f7faf7e15a1c5979eb3989d8c457876cc0c3ad
harshita-yadav8 <harshi.yadav4992@gmail.com>
2026-09-18 13:22:02 +0530

    Delete diagnosticRoutes.js

--- diagnosticRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getQuestions, submitDiagnostic } = require("../controllers/diagnosticController");
-const protect = require("../middleware/authMiddleware");
-
-router.get("/questions", getQuestions);
-router.post("/submit", protect, submitDiagnostic);
-
-module.exports = router;
